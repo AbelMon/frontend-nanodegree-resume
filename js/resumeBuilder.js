46903b887e57b866ec66ca738a2f8efb6222ff42
Abel Monterroso <marvinabelmon@gmail.com>
2015-06-25 18:29:56 -0600

    Primera version del proyecto

---
+++ js/resumeBuilder.js
@@ -0,0 +1,67 @@
+var bio = {
+    "name": "Abel Monterroso",
+    "role": "Ninja",
+    "contacts": {
+        "mobile": "1730",
+        "email": "marvinabelmon@gmail.com",
+        "github": "github.com/AbelMon",
+        "twitter": "@Yolo",
+        "location": "Guatemala"
+    },
+    "welcomeMessage": "Welcome to my page! You will find important information about me. You do not extortion!",
+    "skills": ["Sleep 14 hours straight.", "Touching bugs without feeling disgust.", "Rasengan", "Perfect chakra control.", "Yin-yang element", "Katon", "Fūton", "Raiton"],
+    "biopic": "http://www.topdesignmag.com/wp-content/uploads/2014/03/cute-smiling-animals-251.jpg",
+    display: function() {
+        // body...
+    }
+};
+
+var education = {
+    "schools": [
+        {
+        "name": "Ninja Academy",
+        "location": "Konoha",
+        "degree": "Jōnin",
+        "majors": ["Kakashi Hatake", "Captain Yamato", "Hagoromo Ōtsutsuki"],
+        "dates": 2014,
+        "url": "http://es.naruto.wikia.com/wiki/Naruto_Wiki"
+        }
+    ],
+    "onlineCourses": [
+    "title": "ANBU",
+    "school": "Ninja Academy",
+    "date": 2014,
+    "url": "http://www.topdesignmag.com/wp-content/uploads/2014/03/cute-smiling-animals-251.jpg"
+    ],
+    display: function() {
+
+    }
+};
+
+var work = {
+    jobs: [
+        {
+            "employer": "Hagoromo Ōtsutsuki",
+            "title": "Ambu Hunter",
+            "location": "Otogakure",
+            "date": "05/05/2015",
+            "description": "Find information about Kaguya."
+        }
+    ],
+    display: function() {
+    }
+};
+
+var projects = {
+    projects: [
+        {
+            "title": "Fourth ninja war",
+            "dates": "06/06/2015",
+            "description": "illing members of Akatsuki and destroy Kaguya.",
+            "images": ["http://sp8.fotolog.com/photo/24/45/92/emozoe_zoy_emo17/1320934373759_f.jpg", "http://fc04.deviantart.net/fs71/i/2012/351/2/d/juubi_by_themnaxs-d5od02e.png", "http://images6.fanpop.com/image/photos/37300000/-Kaguya-Attack-s-naruto-shippuuden-37396410-1330-601.png"],
+        }
+    ],
+    display: function() {
+
+    }
+};
